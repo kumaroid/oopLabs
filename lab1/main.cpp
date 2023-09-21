c63d53bcc0852b756aa6ce26b51aafb2c5a84f53
kumaroid <113765449+kumaroid@users.noreply.github.com>
2023-09-21 23:59:04 +0300

    Create main.cpp

---
+++ lab1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double conversionV(double inch)
+{
+    return pow(inch * 0.083333333333333, 3);
+}
+
+int box_capacity(int length, int width, int heigth) {
+
+    int storeV = length * width * heigth;
+    
+    return storeV / conversionV(16);
+
+}
+
+int main() {
+
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    int res = box_capacity(a,b,c); 
+
+    cout << res << endl;
+
+    return 0;
+}
