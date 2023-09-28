5c31eb39fed73aaf554727b5da88e37474000249
kumar <andr-ushka2@yandex.ru>
2023-09-28 11:47:52 +0300

    meow moew

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,19 +3,19 @@
 
 TEST(test_01, basic_test_set)
 {
-    ASSERT_TRUE(result("0 0 0 ") == "0");
+    ASSERT_TRUE(("0 0 0 ") == "0");
 
     
 }
 
 TEST(test_02, basic_test_set)
 {
-    ASSERT_TRUE(result("10 20 30") == "2531");
+    ASSERT_TRUE(("10 20 30") == "2531");
 }
 
 TEST(test_03, basic_test_set)
 {
-    ASSERT_TRUE(result("aabacbaa") == "bbabcabb");
+    ASSERT_TRUE(("16 64 32") == "13824");
 }
 
 int main(int argc, char **argv) {
